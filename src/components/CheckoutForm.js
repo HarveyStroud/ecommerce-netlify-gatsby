1e89dc5279f5b776488c2194e7d934c1d621e4bd
Harvey Stroud <harvey.stroud@gmail.com>
2019-08-23 17:27:34 +0200

    Add Stripe payment processing

---
+++ src/components/CheckoutForm.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import {CardElement, injectStripe} from 'react-stripe-elements';
+
+/**
+ * @ref https://stripe.com/docs/recipes/elements-react
+ */
+class CheckoutForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {complete: false};
+    this.submit = this.submit.bind(this);
+  }
+
+  async submit(ev) {
+    // User clicked submit
+    let {token} = await this.props.stripe.createToken({name: "Name"});
+this.stripeEmail = 'harveys@live.fr';
+const stripeData = { token, stripeEmail: this.stripeEmail };
+    let response = await fetch("/index", {
+        method: "POST",
+        headers: {"Content-Type": "text/plain"},
+        body: stripeData
+    });
+
+    if (response.ok) {
+        this.setState({complete: true});
+        console.log("Purchase Complete!")
+    }
+}
+
+  render() {
+    if (this.state.complete) return <h1>Purchase Complete</h1>;
+
+    return (
+      <div className="checkout">
+        <p>Would you like to complete the purchase?</p>
+        <CardElement />
+        <button onClick={this.submit}>Send</button>
+      </div>
+    );
+  }
+}
+
+export default injectStripe(CheckoutForm);
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,14 +1,34 @@
-import React from "react"
+import React from 'react'
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+
+import {Elements, StripeProvider} from 'react-stripe-elements'
+import CheckoutForm from '../components/CheckoutForm'
+
+function buySomethingCheapAndCheerful() {
+  alert("Buying SomethingCheapAndCheerful!")
+}
 
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
+    <button onClick={buySomethingCheapAndCheerful}>Buy something cheap 'n' cheerful...</button>
+    <br />
+    <br />
+    <StripeProvider apiKey="pk_test_NtNh5ztyxJ1Qi7KXNaWPVTdm00Facs4hnt">
+      <div className="example">
+        <h1>React Stripe Elements Example</h1>
+        <Elements>
+          <CheckoutForm />
+        </Elements>
+      </div>
+    </StripeProvider>
+    <br />
+    <br />
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
