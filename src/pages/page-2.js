5d5f8ff19f0c787dddce18ebf3153114686c0206
Harvey Stroud <harvey.stroud@gmail.com>
2019-08-24 18:12:49 +0200

    Correct page-2 render method

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -11,26 +11,41 @@ function buySomethingCheapAndCheerful() {
   alert("Buying SomethingCheapAndCheerful!")
 }
 
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <button onClick={buySomethingCheapAndCheerful}>Buy something cheap 'n' cheerful...</button>
-    <br />
-    <br />
-    <StripeProvider apiKey="pk_test_NtNh5ztyxJ1Qi7KXNaWPVTdm00Facs4hnt">
-      <div className="example">
-        <h1>React Stripe Elements Example</h1>
-        <Elements>
-          <InjectedCheckoutForm />
-        </Elements>
-      </div>
-    </StripeProvider>
-    <br />
-    <br />
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const SecondPage = () => {
+  constructor() {
+    super();
+    this.state = {stripe: null};
+  }
+
+  componentDidMount() {
+    // Create Stripe instance in componentDidMount
+    // (componentDidMount only fires in browser/DOM environment)
+    this.setState({stripe: window.Stripe('pk_test_NtNh5ztyxJ1Qi7KXNaWPVTdm00Facs4hnt')});
+  }
+
+  render() {
+    return (
+      <Layout>
+        <SEO title="Page two" />
+        <h1>Hi from the second page</h1>
+        <p>Welcome to page 2</p>
+        <button onClick={buySomethingCheapAndCheerful}>Buy something cheap 'n' cheerful...</button>
+        <br />
+        <br />
+        <StripeProvider stripe={this.state.stripe}>
+          <div className="example">
+            <h1>React Stripe Elements Example</h1>
+            <Elements>
+              <InjectedCheckoutForm />
+            </Elements>
+          </div>
+        </StripeProvider>
+        <br />
+        <br />
+        <Link to="/">Go back to the homepage</Link>
+      </Layout>
+    )
+  }
+}
 
 export default SecondPage
